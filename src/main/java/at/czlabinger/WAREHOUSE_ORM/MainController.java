60a58345d65e17bedce6656a1ad2dcc5bd63cc68
czlabinger <czlabinger@student.tgm.ac.at>
2024-04-16 10:34:04 +0200

    gkue done

---
+++ src/main/java/at/czlabinger/WAREHOUSE_ORM/MainController.java
@@ -0,0 +1,28 @@
+package at.czlabinger.WAREHOUSE_ORM;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.*;
+
+@Component
+@RestController
+@AllArgsConstructor
+public class MainController {
+
+    private final UserRepository userRepository;
+
+    @PostMapping(path="/add")
+    public @ResponseBody String addNewUser (@RequestParam String email, @RequestParam String password) {
+
+        User n = new User();
+        n.setEmail(email);
+        n.setPassword(password);
+        userRepository.save(n);
+        return "Saved";
+    }
+
+    @GetMapping(path="/all")
+    public @ResponseBody Iterable<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+}
